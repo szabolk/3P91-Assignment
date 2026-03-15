c1c23a113123870a01acf4701bafb8b1aa3b22d2
szabolk <luke.szabo@hotmail.com>
2026-03-14 21:21:26 -0400

    finished the saving functionality and added XSD schema support

--- 3P91A3_7698368_8330508/A3Code/src/UtilThings/VillageSaver.java
+++ 3P91A3_7698368_8330508/A3Code/src/UtilThings/VillageSaver.java
@@ -10,15 +10,38 @@ import javax.xml.transform.stream.StreamResult;
 import java.io.*;
 
 public class VillageSaver {
+    //from the xml example code
+    private static final String JAXP_SCHEMA_LANGUAGE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
+    private static final String W3C_XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
+    private static final String JAXP_SCHEMA_SOURCE = "http://java.sun.com/xml/jaxp/properties/schemaSource";
 
+    /**
+     * Used whenever we want to add a child to an already existing parent element
+     * @param document - The overall document (DOM structure)
+     * @param parent - The parent we want to add a child element to
+     * @param tag - The XML tag associated with the element
+     * @param value - The value we want to save
+     */
     public static void addChildElement(Document document, Element parent, String tag, String value) {
         Element childElement = document.createElement(tag);
         childElement.appendChild(document.createTextNode(value));
         parent.appendChild(childElement);
     }
 
+    /**
+     * Once the player chooses to exit the game, the game will run this to save the state of the player's village before they quit, and then use the XML structure in the file to load the village when the
+     * player decides to play again.
+     * @param village - the player's village
+     * @param filePath - the XML file where the player's village will be saved
+     * @throws ParserConfigurationException - throws if an issue with DocumentBuilder or DocumentBuilderFactory
+     * @throws TransformerException - throws if an error occurs during the transformer work
+     */
     public static void villageToXML(Village village, String filePath) throws ParserConfigurationException, TransformerException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        factory.setValidating(true);
+        factory.setAttribute(JAXP_SCHEMA_LANGUAGE, W3C_XML_SCHEMA);
+        factory.setAttribute(JAXP_SCHEMA_SOURCE, "villageSchema.xsd");
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document document = builder.newDocument();
 
@@ -30,26 +53,70 @@ public class VillageSaver {
         addChildElement(document, rootElement, "villageHallLevel", String.valueOf(village.getVillageHall().getStats().level()));
         addChildElement(document, rootElement, "guardedUntil", String.valueOf(village.getGuardedUntil()));
 
-        //add resources to the structure
+        //saves the player's win/loss count for both attacking and defending
+        Element playerAttackStats = document.createElement("playerAttackStats");
+        rootElement.appendChild(playerAttackStats);
+        addChildElement(document, playerAttackStats, "wins", String.valueOf(village.getOwner().getWinTotal()));
+        addChildElement(document, playerAttackStats, "losses", String.valueOf(village.getOwner().getLossTotal()));
+
+        Element playerDefenceStats = document.createElement("playerDefenceStats");
+        rootElement.appendChild(playerDefenceStats);
+        addChildElement(document, playerDefenceStats, "wins", String.valueOf(village.getOwner().getDefenseVictory()));
+        addChildElement(document, playerDefenceStats, "losses", String.valueOf(village.getOwner().getDefenseLosses()));
+
+        //saves the player resources
         Element resources = document.createElement("resources");
         rootElement.appendChild(resources);
         addChildElement(document, resources, "gold", String.valueOf(village.getResources().getGold()));
         addChildElement(document, resources, "iron", String.valueOf(village.getResources().getIron()));
         addChildElement(document, resources, "lumber", String.valueOf(village.getResources().getLumber()));
 
+        //saves the player inhabitants
         Element inhabitants = document.createElement("inhabitants");
         rootElement.appendChild(inhabitants);
         for (Inhabitant inhabitant : village.getInhabitants()) {
             Element inhabitantNode = document.createElement("inhabitant");
-            addChildElement(document, inhabitantNode, "unitType", String.valueOf(inhabitant.getEntityType()));
+            addChildElement(document, inhabitantNode, "type", String.valueOf(inhabitant.getEntityType()));
             addChildElement(document, inhabitantNode, "level", String.valueOf(inhabitant.getStats().level()));
             inhabitants.appendChild(inhabitantNode);
         }
 
+        //saves the player buildings
+        Element buildings = document.createElement("buildings");
+        rootElement.appendChild(buildings);
+        for (Building building : village.getBuildings()) {
+            Element buildingNode = document.createElement("building");
+            addChildElement(document, buildingNode, "type", String.valueOf(building.getEntityType()));
+            addChildElement(document, buildingNode, "level", String.valueOf(building.getStats().level()));
+            buildings.appendChild(buildingNode);
+        }
+
+        //save the build queue status before the player quits the game
+        Element buildQueue = document.createElement("buildQueue");
+        rootElement.appendChild(buildQueue);
+        for (Village.QueueTask task : village.getBuildQueue()) {
+            Element queueNode = document.createElement("task");
+            addChildElement(document, queueNode, "type", String.valueOf(task.getType()));
+            addChildElement(document, queueNode, "completionTime", String.valueOf(task.getCompletionTime()));
+            addChildElement(document, queueNode, "buildingToUpgrade", String.valueOf(task.getExistingBuilding()));
+            buildQueue.appendChild(queueNode);
+        }
+
+        //save the train queue status before the player quits the game
+        Element trainQueue = document.createElement("trainQueue");
+        rootElement.appendChild(trainQueue);
+        for (Village.QueueTask task : village.getTrainQueue()) {
+            Element queueNode = document.createElement("task");
+            addChildElement(document, queueNode, "type", String.valueOf(task.getType()));
+            addChildElement(document, queueNode, "completionTime", String.valueOf(task.getCompletionTime()));
+            trainQueue.appendChild(queueNode);
+        }
+
+        //actually put the saved info into a file through transformer
         TransformerFactory tfactory = TransformerFactory.newInstance();
         Transformer transformer = tfactory.newTransformer();
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "5");
         DOMSource source = new DOMSource(document);
         StreamResult result = new StreamResult(new File(filePath));
         transformer.transform(source, result);
